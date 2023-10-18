609900241f7e856d1f6b3f00be9ede8ab9ff0a78
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-18 14:33:35 +0300

    second task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const list = document.querySelector('.gallery');
 
 const createGallery = galleryItems.map(({original, preview, description}) => `
