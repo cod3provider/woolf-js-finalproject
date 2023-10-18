082342915a5b79b2a59118e842b8d7a98b119052
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-18 13:37:48 +0300

    add markup for gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+const createGallery = galleryItems.map(image => `
+        <li className="gallery__item">
+            <a className="gallery__link" href=${image.original}>
+                <img 
+                    className="gallery__image" 
+                    src=${image.preview} 
+                    alt=${image.description}
+                />
+            </a>
+        </li>
+    `).join('');
+
+list.insertAdjacentHTML('beforeend', createGallery)
