85d7e807744991e7e81d8a1f31f26cd9d91a1b9a
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-18 12:17:22 +0300

    create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,20 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const list = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(item => `
+    <li class="gallery__item">
+        <a class="gallery__link" href=${item.original}>
+            <img 
+                class="gallery__image" 
+                src=${item.preview} 
+                alt=${item.description}
+                data-source=${item.original}
+            />
+        </a>
+    </li>
+`).join('');
+
+list.insertAdjacentHTML('afterbegin', galleryMarkup);
+
